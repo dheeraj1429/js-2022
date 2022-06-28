b2d616d0e32c4f49b9ba010db39d79450f3a21ee
dheeraj1429 <dheerajsingh1429@gmail.com>
2022-06-28 16:17:05 +0530

    new projects

---
+++ busNet/public/javascript/app.js
@@ -0,0 +1,30 @@
+// 1 - strings ****************************************************
+
+/*
+     3 way to type strings '', "", ``
+
+     1 - ' '' ' 2 - " '' " 3 - ``
+ */
+
+// const z = "hello my name us dheeraj";
+
+// const a = "this is a string" + " " + z;
+
+// console.log(a);
+
+// const a = "dheeraj";
+// const b = "this is me";
+
+// const z = `${a} ${b}`;
+
+// console.log(z);
+
+//  string methods **************************************************
+
+// const h = "this is me";
+
+// console.log(h.length);
+// console.log(h.includes("this"));
+// console.log(h.toLocaleLowerCase());
+// console.log(h.toUpperCase());
+// console.log(h.toLowerCase());
